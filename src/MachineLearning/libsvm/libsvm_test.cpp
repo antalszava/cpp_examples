eced9e223e40f9a9a8162fa69c570f068436fc9d
Trevor Vincent <tvincent@cita.utoronto.ca>
2018-04-24 01:19:12 -0400

    Add svm test

---
+++ src/MachineLearning/libsvm/libsvm_test.cpp
@@ -0,0 +1,12 @@
+#include "svm.h"
+#include <ctype.h>
+#include <stdlib.h>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+/* Compile with g++ -o libsvm_test libsvm_test.cpp svm.cpp */
+int main(int argc, char *argv[])
+{
+  return 0;
+}
