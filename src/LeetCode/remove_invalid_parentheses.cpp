0c0f557e313960b98c36133065bea95955b73cc6
Trevor Vincent <tvincent@cita.utoronto.ca>
2019-10-14 17:25:16 -0400

    more leetcode

--- src/LeetCode/remove_invalid_parentheses.cpp
+++ src/LeetCode/remove_invalid_parentheses.cpp
@@ -3,6 +3,8 @@
 #include <stack>
 #include <stdio.h>
 #include <string.h>
+#include <vector>
+#include <set>
 // #include <queue>
 
 bool is_valid(const std::string & str){
@@ -12,11 +14,6 @@ bool is_valid(const std::string & str){
     return 1;
   }
 
-  // if (str.size() % 2 != 0){
-      // return 0;
-  // }
-  
-  
   for (int i = 0; i < str.size(); i++){
     if (str[i] == '('){
       left.push(')');
@@ -28,8 +25,8 @@ bool is_valid(const std::string & str){
       left.push('}');
     }
     else if (str[i] == ')' ||
-	str[i] == '}' ||
-	str[i] == ']'){
+	     str[i] == '}' ||
+	     str[i] == ']'){
       if (left.size() == 0){
 	return 0;
       }
@@ -40,9 +37,6 @@ bool is_valid(const std::string & str){
 	return 0;
       }
     }
-    // else {
-      // return 0;
-    // }
   }
 
   if (left.size() == 0){
@@ -53,45 +47,91 @@ bool is_valid(const std::string & str){
   }
 }
 
-std::vector<string> remove_from_invalid(const std::string & str){
 
-  std::stack<char> left;
-  std::queue<char> right;
-
-  str::vector<string> valid_substrings;
-
-  int left_count == 0;
-  int right_count == 0;
+int is_valid_char(const std::string & str, char left_char, char right_char){
+  if (str.size() == 0){
+    return 0;
+  }
+  int left_count = 0;
+  int right_count = 0;
   
   for (int i = 0; i < str.size(); i++){
-    if (str[i] == '(' ||
-	str[i] == '{' ||
-	str[i] == ']'){
+    if (str[i] == left_char){
       left_count++;
     }
-    if (str[i] == ')' ||
-	str[i] == '}' ||
-	str[i] == ']'){
+    if (str[i] == right_char){
       right_count++;
     }  
   }
-  int diff = abs(left_count - right_count);
-
-  if (left_count > right_count){
-    //try remove all left string combinations until valid
-  }
-
-  if (right_count > left_count){
-
-  }
-  
-  return valid_strings;
+  return left_count - right_count;
 }
 
+std::set<std::string> remove_char_from_strings
+(
+ const std::set<std::string> & strs,
+ int num_rem,
+ char rem
+)
+{
+  
+  std::set<std::string> ret_strs;
+  for (auto i : strs){
+    for (int j = 0; j < i.size(); j++){
+      if (i[j] == rem){
+	std::string str = i;
+	str.erase(j, 1);
+	ret_strs.insert(str);
+      }
+    }
+  }
 
+
+  // std::cout << std::endl;
+  // std::cout << "REMOVE_CHAR_FROM_STRINGS" << std::endl;
+  // for (int i = 0; i < strs.size(); i++){
+  //     std::cout << strs[i] << std::endl;
+  // }
+  // std::cout << "char = " << rem << std::endl;
+  // std::cout << "num_rem = " << num_rem << std::endl;
+  // for (int i = 0; i < ret_strs.size(); i++){
+  //     std::cout << ret_strs[i] << std::endl;
+  // }
+  // std::cout << std::endl;
+
+  
+  if (num_rem > 1){
+    ret_strs = remove_char_from_strings(ret_strs, num_rem - 1, rem);
+  }
+  return ret_strs;
+}
 
 int main(int argc, char *argv[])
 {
-  std::cout << is_valid("(aaaa){2}[") << std::endl;
+
+  std::string str("[[]]](aaaa){2}[[]]]");
+  std::set<std::string> strings;
+  strings.insert(str);
+  
+  // std::cout << is_valid("(aaaa){2}[") << std::endl;
+  int diff0 = is_valid_char(str, '(', ')');
+  if (diff0 != 0)
+    strings = remove_char_from_strings(strings, abs(diff0), (diff0 > 0) ? '(' : ')');
+  
+  int diff1 = is_valid_char(str, '{', '}');
+  if (diff1 != 0)
+    strings = remove_char_from_strings(strings, abs(diff1), (diff1 > 0) ? '{' : '}');
+  
+  int diff2 = is_valid_char(str, '[', ']');
+  if (diff2 != 0)
+    strings = remove_char_from_strings(strings, abs(diff2), (diff2 > 0) ? '[' : ']');
+  
+
+  std::cout << "FINISHED" << std::endl;
+  for (auto i : strings){
+    if (is_valid(i)){
+      std::cout << i << std::endl;
+    }
+  }
+  
   return 0;
 }
