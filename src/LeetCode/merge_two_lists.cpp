d497b91594d1b0c7bb7fb7be5f51d78209a219a7
Trevor Vincent <tvincent@cita.utoronto.ca>
2019-10-18 19:28:19 -0400

    more leet

--- src/LeetCode/merge_two_lists.cpp
+++ src/LeetCode/merge_two_lists.cpp
@@ -96,7 +96,7 @@ int main(int argc, char *argv[])
   
 
 
-//better
+//better recursion
 ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) {
             return l2;
@@ -113,4 +113,30 @@ ListNode mergeTwoLists(ListNode l1, ListNode l2) {
             return l2;
         }
 
+    }
+
+
+
+//better iteration
+ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        // maintain an unchanging reference to node ahead of the return node.
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // exactly one of l1 and l2 can be non-null at this point, so connect
+        // the non-null list to the end of the merged list.
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
     }
