795eef4c26e3055f9b92fd0c6bee0580b4e43dc5
Trevor Vincent <tvincent@cita.utoronto.ca>
2019-11-17 15:23:13 -0500

    more leet

---
+++ src/LeetCode/680_valid_palindrome_2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:    
+    bool checksubstring(string s, int i, int j){
+        for (int k = i; k <= i + (j-i)/2; k++){
+            if (s[k] != s[j - k + i]) 
+                return false;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        for (int i = 0; i < s.size()/2; i++){
+            if (s[i] != s[s.size()-1-i]){                
+                return checksubstring(s, i+1, s.size()-1-i) || checksubstring(s,i, s.size()-2-i);
+            }
+        }
+        return true;
+    }
+};
