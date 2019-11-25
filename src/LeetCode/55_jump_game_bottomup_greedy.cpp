a9509b5d3efd3992b0a85de15a10a8b607df0053
Trevor Vincent <tvincent@cita.utoronto.ca>
2019-11-25 16:21:28 -0500

    more leet

---
+++ src/LeetCode/55_jump_game_bottomup_greedy.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  bool canJump(vector<int> nums) {
+    int lastPos = nums.size() - 1;
+    for (int i = nums.size() - 1; i >= 0; i--) {
+      if (i + nums[i] >= lastPos) {
+	lastPos = i;
+      }
+    }
+    return lastPos == 0;
+  }
+};
