9861824a756ae1722d63586406483dc41f037eae
Trevor Vincent <tvincent@cita.utoronto.ca>
2019-12-10 00:22:22 -0500

    more leet

--- src/LeetCode/202_happy_number.cpp
+++ src/LeetCode/202_happy_number.cpp
@@ -1,3 +1,7 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+
 class Solution {
 public:
     
@@ -5,21 +9,29 @@ public:
         
         if (n == 1) return true;
 
+	map<int,int> history;
+	
         int happy = 0;
         int num = n;
         
-        while(1){
+        while(num < INT_MAX){
             int temp = num;
             int sum = 0;
+            
+	    if (history.find(num) != history.end()) return false;
+	        history.emplace(std::make_pair(num,1));
             
             while (temp != 0){
                 int dig = temp % 10;
                 sum += dig*dig;
                 temp /= 10;
             }
-            
+
+	    cout << sum << endl;
+	    cout << num << endl;
+
             if (sum == 1) return true;
-            if (sum == n) return false;
+            //if (sum == n) return false;
             num = sum;
         }
         
@@ -32,7 +44,7 @@ public:
 
 int main(int argc, char *argv[])
 {
-  std::cout << Solution().addBinary("11","1") << std::endl;
+  std::cout << Solution().isHappy(19) << std::endl;
   return 0;
 }
 
